3d1e37bc9f1a7efd0ac292b7d7ce9705842c2ca9
Axel Irias <axelirias@Axels-MacBook-Air.local>
2023-03-23 16:54:39 -0600

    adding seeds

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO deparments (deparments_name)
+VALUES
+('Sales'),
+('Engineering'),
+('Legal'),
+('Accounting'),
+('Hr');
+
+INSERT INTO role (title, salary, deparments_id)
+VALUES
+
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
