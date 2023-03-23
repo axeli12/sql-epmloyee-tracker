2a2b2fe5be9be92cea6909cbdf6d431c928ce702
Axel Irias <axelirias@Axels-MacBook-Air.local>
2023-03-23 17:32:14 -0600

    added roles to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,6 +8,18 @@ VALUES
 
 INSERT INTO role (title, salary, deparments_id)
 VALUES
+('Lead Sales', 100000, 1),
+('Lead Engineer', 150000, 2),
+('JR Engineer', 70000, 2),
+('Paralegal', 80000, 3),
+('Head Lawyer', 150000,3),
+('jr Accountant', 50000, 4),
+('Accountant', 80000, 4),
+('Lead accountant', 100000, 4),
+('Lead Hr', 120000, 5),
+('Assistant', 50000, 5);
+
+
 
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
